fa0d7eff8beba2f541a09af5aa0b8ee355fdeca4
henriqrsg <luizhrsguedes@gmail.com>
2026-02-23 11:42:56 -0300

    Atualizando exercício 028

--- PythonCeVexercicios/ex028.py
+++ PythonCeVexercicios/ex028.py
@@ -2,9 +2,21 @@
 
 import random 
 
+import os
+
 num = random.randint(0, 5)
 
-numero_escolhido = int(input('Escolha um número entre 0 e 5: '))
+nome_programa = 'VAMOS TESTAR SUA SORTE'
+
+def interface():
+    os.system('cls')
+    print(70 *  '=')
+    print('{:^70}'.format(nome_programa))
+    print(70 *  '=')
+    numero_escolhido = int(input('Escolha um número entre 0 e 5: '))
+    return numero_escolhido
+
+numero_escolhido = interface()
 
 if numero_escolhido == num:
     print('\033[1;32mParabéns! Você acertou o número!\033[m')
